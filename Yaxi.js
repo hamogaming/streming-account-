d23517775df138cd48c51d9fdcd4adac3821825d
hamogaming <82784417+hamogaming@users.noreply.github.com>
2021-07-12 09:42:09 +0300

    Update Yaxi.js

--- Yaxi.js
+++ Yaxi.js
@@ -14,15 +14,15 @@ Yaxi.on("ready", () => {
   setInterval(function() {
     let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
     Yaxi.user.setActivity(STREAMING, {      
-    type: "STREAMING", 
+    type: "DEVx7AMO", 
     url: "https://www.twitch.tv/faith"
     });
   }, 2000);
 });
 ///////////////////////
 Yaxi.on("ready", () => {
-    var join = Yaxi.channels.get(""); /// id voice
+    var join = Yaxi.channels.get("860270669374685258"); /// id voice
     if (join) join.join();
   });
 //////////////// tokn lera da bne 
-Yaxi.login("");
+Yaxi.login("ODY0MDE0OTQ2ODEwMzMxMTM3.YOvSnw.SED7OUtpHC2g0tSf2wdJpGRCDI8");
